2f47a9dbebe6e0cb31b473e9d5ddeedd7c8fef1b
Charlie-BrownGT <108026345+Charlie-BrownGT@users.noreply.github.com>
2023-08-11 18:50:00 +0300

    update

---
+++ prime_number.py
@@ -0,0 +1,26 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    
+    return True
+
+def generate_primes(limit):
+    primes = []
+    for num in range(2, limit):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+limit = int(input("Enter the upper limit for prime number generation: "))
+prime_list = generate_primes(limit)
+print("Prime numbers up to", limit, "are:", prime_list)
